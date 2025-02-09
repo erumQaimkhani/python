0c35b30f53f24c99c770a5be0ff04dbc52c61c47
Erum azeemi qaimkhani <160758395+erumQaimkhani@users.noreply.github.com>
2025-02-09 17:14:34 +0500

    Add files via upload

---
+++ concatination.py
@@ -0,0 +1,13 @@
+
+
+favorite_Prophet="Holy Prophet Muhammad (PBUH)"
+print(favorite_Prophet)
+
+favroite_Prophet="Holy Prophet Muhammad (PBUH)"
+print("favorite_Prophet")
+
+# name="Erum Azeemi Qaimkhani"
+# Father_name="Muhammad Abdul Aziz Khan"
+# gender="Female"
+# print(name,Father_name,gender)
+
