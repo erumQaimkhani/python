88bdd94f0acedf990d88d1ea3b7ec210772e7830
Erum azeemi qaimkhani <160758395+erumQaimkhani@users.noreply.github.com>
2025-02-26 15:24:16 +0500

    Add files via upload

---
+++ growth-mind-set.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("Growth Mindset Challenge")
+st.header("What's a Growth Mindset?")
+st.write("""
+A growth mindset is the belief that you can learn and grow by working hard and learning from mistakes.
+         
+# # 🎬 YouTube Video
+ st.video('https://www.youtube.com/watch?v=XRqXjiffPlE')
+You can:
+- **Embrace Challenges:** Treat problems as chances to learn.
+- **Learn from Mistakes:** It's okay to mess up! That's how you learn.
+- **Keep Trying:** Don’t give up easily.
+- **Celebrate Effort:** Be proud of trying, not just winning.
+""")
+st.subheader("Ready to Grow?")
+st.button("I'm In!")
